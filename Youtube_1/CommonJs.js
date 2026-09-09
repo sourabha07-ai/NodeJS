cf9dcd4d3ec0c84ea4a34471c94b0422d8b643aa
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-10 03:56:10 +0530

    Common Js and UseCommon.js

---
+++ Youtube_1/CommonJs.js
@@ -0,0 +1,6 @@
+export const math_function = (a,b) =>{
+        console.log("ADD:",a+b);
+        console.log("SUB:",a-b);
+        console.log("MULT:",a*b);
+        console.log("DIV:",a/b);
+};
---
+++ Youtube_1/UseCommonJS_file.js
@@ -0,0 +1,4 @@
+// const m = require('./CommonJs.js');
+import { math_function } from "./CommonJs.js";
+
+console.log(math_function(10,11));
