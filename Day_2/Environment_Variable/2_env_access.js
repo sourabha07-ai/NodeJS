57a73cc4e5fc9b0440cdec7ff5c07409006cd4a7
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-11 05:16:25 +0530

    Environment variable topic

---
+++ Day_2/Environment_Variable/2_env_access.js
@@ -0,0 +1,8 @@
+// const dotenv  = require('dotenv');
+// dotenv.config();
+
+console.log(`PORT: ${process.env.PORT}`);
+console.log(`NAME: ${process.env.NAME}`);
+console.log(`MODE: ${process.env.MODE}`);
+
+// console.log(process.env);
---
+++ Day_2/Environment_Variable/3_env_typeof.js
@@ -0,0 +1,5 @@
+const port =Number(process.env.PORT);
+console.log(typeof port);
+
+// cmd: $ node --env-file=.env 3_env_typeof.js 
+// answer: number
