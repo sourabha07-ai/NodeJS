8becb139189b77df522c5eaa975d1f5fd7c84e03
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-10 08:19:05 +0530

    4_path.js

--- Youtube_1/Create_a_Server/4_path.js
+++ Youtube_1/Create_a_Server/4_path.js
@@ -1,6 +1,7 @@
 import path  from "node:path";
 
-const file_path = "d:/Nodejs2026/Youtube_1/Create_a_Server/4_path.js";
+// const file_path = "d:/Nodejs2026/Youtube_1/Create_a_Server/4_path.js";
+const file_path = path.join("users","Documents","File.txt"); //users/documents/file.txt
 
 console.log(path.basename(file_path)); //4_path.js
 console.log(path.dirname(file_path)); //d:/Nodejs2026/Youtube_1/Create_a_Server
