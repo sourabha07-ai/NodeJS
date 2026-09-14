0c8a9d9bda3e169f689dc0b0b97eab774878049c
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-15 02:58:43 +0530

    Second Server Update

--- Day_4(MangoDB)/Servers/server2.js
+++ Day_4(MangoDB)/Servers/server2.js
@@ -6,12 +6,17 @@ const second_Server = express();
 const PORT = process.env.PORT;
 
 second_Server.get('/',async(req,res)=>{
-        res.send("Hello Sourabha!");
+        await res.send("Hello Sourabha!");
         console.log("Get Server is Running...✅");
 
 });
 
+second_Server.get('/home',async(req,res)=>{
+        await res.send("This is Home Page 🏠");
+        console.log("Home page created✅");
+});
+
 
 second_Server.listen(PORT,()=>{
-        console.log(`Second server is Running in http;//localhost:${PORT}`);
+        console.log(`Second server is Running in http://localhost:${PORT}`);
 })
