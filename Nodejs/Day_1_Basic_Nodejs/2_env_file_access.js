50c812dd98f05b3c21ea27806c74a39a399db0cb
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-25 02:29:18 +0530

    2_env_file_access.js

---
+++ Nodejs/Day_1_Basic_Nodejs/2_env_file_access.js
@@ -0,0 +1,5 @@
+console.log("PORT:",process.env.PORT);
+console.log("Username:",process.env.USERNAME);
+console.log("Password:",process.env.PASSWORD);
+console.log("MONGODB URL:",process.env.URL);
+console.log("Link:",process.env.LINK);
