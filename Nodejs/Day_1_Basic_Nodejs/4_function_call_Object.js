fcaf1acb93db7f8c16d7da389effdc259b678930
Sourabha Jena <sourabhajena2003@gmail.com>
2026-09-25 03:06:41 +0530

    4_function_call_Object.js

---
+++ Nodejs/Day_1_Basic_Nodejs/4_function_call_Object.js
@@ -0,0 +1,4 @@
+import {User} from "./user2.js"
+
+const importValue = User("Sourabha Jena","Software Developer")
+console.log("User Details:",importValue);
---
+++ Nodejs/Day_1_Basic_Nodejs/user2.js
@@ -0,0 +1,8 @@
+export const User =(name,role)=>{
+     return{
+      id:1,
+      name,
+      role,
+      city:"Bhubaneswar"
+     }
+}
